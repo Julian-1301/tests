75ba78ba59e061a7b3f6a3c9fe19f3fbf0692e0e
Julian van der Linde <lindejc@hva.nl>
2024-08-27 16:44:06 +0200

    return button

--- base.js
+++ base.js
@@ -2,11 +2,12 @@
 const body = document.body;
 
 // Create a new anchor element
-const returnElement = document.createElement('a');
+const returnElement = document.createElement("a");
 
 // Set the attributes and inner HTML for the anchor element
-returnElement.href = '../../index.html'; // Set the href attribute
-returnElement.innerText = 'Return to Home'; // Set the text inside the anchor
+returnElement.href = "../../index.html"; // Set the href attribute
+returnElement.innerText = "Return to Home"; // Set the text inside the anchor
+returnElement.classList.add("return");
 
 // Append the anchor element to the body
 body.appendChild(returnElement);
